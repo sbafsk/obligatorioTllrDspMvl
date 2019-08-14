90e862e5afe29b53aa16adbd09261432a3d3121a
sebahead <razor1836@gmail.com>
2019-08-13 22:03:37 -0300

    avances
    Alta y Baja medio de Pago

--- functions/main.js
+++ functions/main.js
@@ -5,36 +5,15 @@ var db;
 
 $(document).ready(init);
 
-function OpenDataBase(){
-    db = openDatabase('TestDB',1,'guardar test', 1024);
+function OpenDataBase() {
+    db = openDatabase('TestDB', 1, 'guardar test', 1024);
 };
 
 function init() {
-    OpenDataBase();  
+    OpenDataBase();
 };
 
 
-function FuncionesAltaMedioDePago(){
-    // obtengo primer digito para setear la imagen de la tarjeta
-    $("#nroTarjeta").on("input", function() {
-        var nro = document.getElementById("nroTarjeta").value;
-        $("#logoTarjeta").attr("src",IconoNroTarjeta(nro));
-    });
-       
-    function IconoNroTarjeta(nro) {    
-        var src = "";
-        if (nro.charAt(0) == "4"){
-            src = "../img/visa-icon.png";
-        } else if (nro.charAt(0) == "5"){
-            src = "../img/master-icon.png";
-        }
-        return src;
-    };     
-}
-
-function FuncionesBajaMedioDePago() {
-
-}
 
 // funciones OnsenUI
 // manejo pantalla inicia Login/Registrar
@@ -43,10 +22,12 @@ window.fnLogin = {};
 window.fnLogin.loadLogin = function (page) {
     var content = document.getElementById('contentLogin');
     content.load(page);
-    if(sessionStorage.getItem("id") != null) {
-        sessionStorage.setItem("token", null);
-        sessionStorage.setItem("id", null);
+    // intente asignarle null, pero luego al valdar (id != null) siempre da true, JS es raro.
+    if (sessionStorage.getItem("id") != "vacio") {
+        sessionStorage.setItem("token", "vacio");
+        sessionStorage.setItem("id", "vacio");
         console.log("Se limpia el sessionStorage");
+        console.log(sessionStorage.getItem("id"));
     }
 };
 
@@ -60,19 +41,20 @@ window.fn.open = function () {
 };
 
 window.fn.load = function (page) {
-    
+
     var content = document.getElementById('content');
     var menu = document.getElementById('menu');
     content.load(page)
         .then(menu.close.bind(menu));
 
-    if (page == "altaMedioPago.html") {
-        $("#nroTarjeta").ready(FuncionesAltaMedioDePago);
+    if (page == "altaMedioPago.html" || page == "bajaMedioPago.html") {
+        $("#nroTarjeta").ready(MostrarIconoTarjeta);
     }
 
     if (page == "bajaMedioPago.html") {
-        $("#nroTarjeta").ready(FuncionesBajaMedioDePago);
+        ObtenerSaldo();
     }
+
 };
 
 
@@ -97,18 +79,21 @@ function Login() {
             }
         };
 
-        $.ajax(settings).done(function (response) {
-            sessionStorage.setItem("token", response.token);
-            sessionStorage.setItem("id", response.id);       
-            
-            var content = document.getElementById('contentLogin');
-            content.load('appPage.html');
-        });
+        $.ajax(settings)
+            .done(function (response) {
+                console.log("doneLogin");
+                sessionStorage.setItem("token", response.token);
+                sessionStorage.setItem("id", response.id);
 
-        $.ajax(settings).fail(function (response) {
-            console.log(response.responseJSON.mensaje);
-            ons.notification.alert(response.responseJSON.mensaje);
-        });
+                var content = document.getElementById('contentLogin');
+                content.load('appPage.html');
+            })
+
+            .fail(function (response) {
+                console.log("failLogin");
+                console.log(response.responseJSON.mensaje);
+                ons.notification.alert(response.responseJSON.mensaje);
+            });
     } else {
         ons.notification.alert('Los campos no pueden quedar en blanco.');
     }
@@ -124,8 +109,8 @@ function Registrar() {
 
     // valido que los campos tengan valores
     if (username != "" && password != "" && confPassword != "") {
-        
-        if (password === confPassword) {            
+
+        if (password === confPassword) {
             var settings = {
                 "url": "http://oransh.develotion.com/usuarios.php",
                 "method": "POST",
@@ -139,16 +124,19 @@ function Registrar() {
                 }
             };
 
-            $.ajax(settings).done(function (response) {   
-                ons.notification.alert('Registro con Exito<br>Ahora puedes ingresar');
-                var contentLogin = document.getElementById('contentLogin');                
-                contentLogin.load('login.html');
-            });
+            $.ajax(settings)
+                .done(function (response) {
+                    console.log("doneReg");
+                    ons.notification.alert('Registro con Exito<br>Ahora puedes ingresar');
+                    var contentLogin = document.getElementById('contentLogin');
+                    contentLogin.load('login.html');
+                })
 
-            $.ajax(settings).fail(function (response) {
-                console.log(response.responseJSON.mensaje);
-                ons.notification.alert(response.responseJSON.mensaje);
-            });
+                .fail(function (response) {
+                    console.log("failReg");
+                    console.log(response.responseJSON.mensaje);
+                    ons.notification.alert(response.responseJSON.mensaje);
+                });
         } else {
             ons.notification.alert('Las contraseñas deben ser identicas.');
         }
@@ -158,66 +146,138 @@ function Registrar() {
 };
 
 
+function MostrarIconoTarjeta() {
+    // obtengo primer digito para setear la imagen de la tarjeta
+    $("#nroTarjeta").on("input", function () {
+        var nro = document.getElementById("nroTarjeta").value;
+        $("#logoTarjeta").attr("src", IconoTarjeta(nro));
+    });
+}
+
+
+function IconoTarjeta(nro) {
+    var src = "";
+    if (nro.charAt(0) == "4") {
+        src = "../img/visa-icon.png";
+    } else if (nro.charAt(0) == "5") {
+        src = "../img/master-icon.png";
+    }
+    return src;
+};
+
+
+// alta medio de pago
 function AltaMedioPago() {
-
-
     var nroTarjeta = document.getElementById('nroTarjeta').value;
-
-    if (nroTarjeta.length == 16 ) {
+    // el numero de la tarjeta deben ser 16 digitos
+    if (nroTarjeta.length == 16) {
         var settings = {
             "url": "http://oransh.develotion.com/tarjetas.php",
             "method": "POST",
             "timeout": 0,
             "headers": {
-              "token": sessionStorage.getItem("token"),
-              "Content-Type": "application/x-www-form-urlencoded"
+                "token": sessionStorage.getItem("token"),
+                "Content-Type": "application/x-www-form-urlencoded"
             },
             "data": {
-              "id": sessionStorage.getItem("id"),
-              "numero": nroTarjeta
+                "id": sessionStorage.getItem("id"),
+                "numero": nroTarjeta
             }
-          };
-          
-          $.ajax(settings).done(function (response) {
-            console.log(response.responseJSON.mensaje);
-          });
-          
-          $.ajax(settings).fail(function (response) {
-            console.log(response.responseJSON.mensaje);
-          });
+        };
+
+        $.ajax(settings)
+            .done(function (response) {
+                console.log("doneAlta");
+                console.log(response.mensaje);
+                ons.notification.alert(response.mensaje);
+            })
+            .fail(function (response) {
+                console.log("failAlta");
+                console.log(response.responseJSON.mensaje);
+                ons.notification.alert(response.responseJSON.mensaje);
+            });
     } else {
         ons.notification.alert('Debe ingresar 16 digitos.');
     }
-    
+
 };
+
+
+function FuncionesBajaMedioDePago() {
+
+}
+
+function MostrarNumeroTarjeta(nro) {
+
+    $("#nroTarjeta").val(nro);
+    $("#nroTarjeta").prop("disabled", true);
+    $("#logoTarjeta").attr("src", IconoTarjeta(nro));
+
+    if (nro.includes("arjeta")) {
+        $('#EliminarMedioPago').prop("disabled", true)
+    }
+}
 
 function BajaMedioPago() {
 
-    if (console.prompt("Desea eliminar su medio de pago ?")) {
+    if (confirm("Desea eliminar su medio de pago ?")) {
         var settings = {
             "url": "http://oransh.develotion.com/tarjetas.php",
-            "method": "DEL",
+            "method": "DELETE",
             "timeout": 0,
             "headers": {
-              "token": sessionStorage.getItem("token"),
-              "Content-Type": "application/x-www-form-urlencoded"
+                "token": sessionStorage.getItem("token"),
+                "Content-Type": "application/x-www-form-urlencoded"
             },
             "data": {
-              "id": sessionStorage.getItem("id")
+                "id": sessionStorage.getItem("id")
             }
-          };
-          
-          $.ajax(settings).done(function (response) {
-            console.log(response.responseJSON.mensaje);
-          });
-          
-          $.ajax(settings).fail(function (response) {
-            console.log(response.responseJSON.mensaje);
-          });
-    }; 
-    
+        };
+
+        $.ajax(settings)
+            .done(function (response) {
+                console.log("doneBaja");
+                console.log(response.mensaje);
+                ons.notification.alert(response.mensaje);
+                MostrarNumeroTarjeta("Tarjeta eliminada")
+            })
+
+            .fail(function (response) {
+                console.log("failBaja");
+                console.log(response);
+            });
+    };
+
 };
 
+
+function ObtenerSaldo() {
+    var settings = {
+        "url": "http://oransh.develotion.com/tarjetas.php",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "token": sessionStorage.getItem("token"),
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            "id": sessionStorage.getItem("id")
+        }
+    };
+
+    $.ajax(settings)
+        .done(function (response) {
+            console.log("doneGet");
+            console.log(response.numero);
+            MostrarNumeroTarjeta(response.numero);
+
+        })
+        .fail(function (response) {
+            console.log("failGet");
+            console.log(response.responseJSON.mensaje);
+            MostrarNumeroTarjeta(response.responseJSON.mensaje);
+        });
+}
 
 
 
